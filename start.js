b6d24661c45282fca01728cb185130fcc56f0722
Khalil-0 <129640276+Khalil-0@users.noreply.github.com>
2023-06-03 13:28:27 +0100

    Beginning of JavaScript learning

---
+++ start.js
@@ -0,0 +1,30 @@
+let a;
+let b;
+const c = 3;
+
+a=4;
+b=5;
+
+//Creating a function
+function mybucket(){
+    console.log(a+b+c);
+}
+function add(p1,p2,p3,p4){
+    console.log(p1+p2+p3+p4);
+}
+
+//Arrow function
+function sub2 () =>{
+    
+}
+
+const sub = (a,b)=>{
+console.log(a+b);
+}
+
+//calling a function
+//mybucket();
+
+//sub(16,8);
+sub(3,5);
+//add(4,3,2,1);
